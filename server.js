8b3830add1675d35b4539b088d5e44e020214db1
Jonathan Lee <JonathanHSL00@gmail.com>
2018-11-09 22:06:45 -0500

    Eat Da Burger!

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+app.use(express.static("public"));
+
+// body_parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// express_handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Link to controller JS
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
